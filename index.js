9a0c43a05abe0866e7c621308d4c57caf19c397c
EmirSoza <emirhansozan@gmail.com>
2023-05-11 18:40:05 +0200

    initial configuration

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const express = require("express");
+const httpStatus = require("http-status");
+const morgan = require("./config/morgan");
+const { errorConverter, errorHandler } = require("./middlewares/error");
+const ApiError = require("./utils/ApiError");
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+const db = require("@cyclic.sh/dynamodb");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
@@ -20,53 +29,77 @@ app.use(express.urlencoded({ extended: true }))
 // #############################################################################
 
 // Create or Update an item
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
+app.post("/:col/:key", async (req, res) => {
+  console.log(req.body);
 
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+  const col = req.params.col;
+  const key = req.params.key;
+  console.log(
+    `from collection: ${col} delete key: ${key} with params ${JSON.stringify(
+      req.params
+    )}`
+  );
+  const item = await db.collection(col).set(key, req.body);
+  console.log(JSON.stringify(item, null, 2));
+  res.json(item).end();
+});
+
+app.get("/", async (req, res) => {
+  return res.json({ msg: "hello world" }).end();
+});
 
 // Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+app.delete("/:col/:key", async (req, res) => {
+  const col = req.params.col;
+  const key = req.params.key;
+  console.log(
+    `from collection: ${col} delete key: ${key} with params ${JSON.stringify(
+      req.params
+    )}`
+  );
+  const item = await db.collection(col).delete(key);
+  console.log(JSON.stringify(item, null, 2));
+  res.json(item).end();
+});
 
 // Get a single item
-app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+app.get("/:col/:key", async (req, res) => {
+  const col = req.params.col;
+  const key = req.params.key;
+  console.log(
+    `from collection: ${col} get key: ${key} with params ${JSON.stringify(
+      req.params
+    )}`
+  );
+  const item = await db.collection(col).get(key);
+  console.log(JSON.stringify(item, null, 2));
+  res.json(item).end();
+});
 
 // Get a full listing
-app.get('/:col', async (req, res) => {
-  const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-  const items = await db.collection(col).list()
-  console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
-})
+app.get("/:col", async (req, res) => {
+  const col = req.params.col;
+  console.log(
+    `list collection: ${col} with params: ${JSON.stringify(req.params)}`
+  );
+  const items = await db.collection(col).list();
+  console.log(JSON.stringify(items, null, 2));
+  res.json(items).end();
+});
 
-// Catch all handler for all other request.
-app.use('*', (req, res) => {
-  res.json({ msg: 'no route handler found' }).end()
-})
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
 
 // Start the server
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`index.js listening on ${port}`)
-})
+  console.log(`index.js listening on ${port}`);
+});
