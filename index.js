746a6b0ea4aa12a0d76a9ea7d47a563d12bf5157
EmirSoza <emirhansozan@gmail.com>
2023-05-11 23:17:56 +0200

    all endpoints

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const httpStatus = require("http-status");
 const morgan = require("./config/morgan");
 const { errorConverter, errorHandler } = require("./middlewares/error");
 const ApiError = require("./utils/ApiError");
+const jsend = require("jsend");
+require("dotenv").config();
+// Load routes from routes folder
+const participantRoute = require("./routes/participants");
 
 const app = express();
+
 if (process.env.NODE_ENV !== "test") {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
-const db = require("@cyclic.sh/dynamodb");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(jsend.middleware);
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
@@ -28,64 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public', options))
 // #############################################################################
 
-// Create or Update an item
-app.post("/:col/:key", async (req, res) => {
-  console.log(req.body);
-
-  const col = req.params.col;
-  const key = req.params.key;
-  console.log(
-    `from collection: ${col} delete key: ${key} with params ${JSON.stringify(
-      req.params
-    )}`
-  );
-  const item = await db.collection(col).set(key, req.body);
-  console.log(JSON.stringify(item, null, 2));
-  res.json(item).end();
-});
-
-app.get("/", async (req, res) => {
-  return res.json({ msg: "hello world" }).end();
-});
-
-// Delete an item
-app.delete("/:col/:key", async (req, res) => {
-  const col = req.params.col;
-  const key = req.params.key;
-  console.log(
-    `from collection: ${col} delete key: ${key} with params ${JSON.stringify(
-      req.params
-    )}`
-  );
-  const item = await db.collection(col).delete(key);
-  console.log(JSON.stringify(item, null, 2));
-  res.json(item).end();
-});
-
-// Get a single item
-app.get("/:col/:key", async (req, res) => {
-  const col = req.params.col;
-  const key = req.params.key;
-  console.log(
-    `from collection: ${col} get key: ${key} with params ${JSON.stringify(
-      req.params
-    )}`
-  );
-  const item = await db.collection(col).get(key);
-  console.log(JSON.stringify(item, null, 2));
-  res.json(item).end();
-});
-
-// Get a full listing
-app.get("/:col", async (req, res) => {
-  const col = req.params.col;
-  console.log(
-    `list collection: ${col} with params: ${JSON.stringify(req.params)}`
-  );
-  const items = await db.collection(col).list();
-  console.log(JSON.stringify(items, null, 2));
-  res.json(items).end();
-});
+app.use("/participants", participantRoute);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
